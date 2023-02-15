3f79323a14f86b24842cca4dc9bec40f886311eb
Sander Ploegsma <sanderploegsma@gmail.com>
2023-02-15 15:56:21 +0100

    Add version flag

--- cmd/transpose/main.go
+++ cmd/transpose/main.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/sanderploegsma/transpose/pkg/transpose"
 )
 
 var (
-	delimiter  = flag.String("d", "|", "Delimiter to use")
-	trimSpaces = flag.Bool("t", false, "Set to true to trim whitespace")
+	version          = "dev"
+	commit           = "none"
+	date             = "unknown"
+	versionOption    = flag.Bool("v", false, "Prints current version")
+	delimiterOption  = flag.String("d", "|", "Delimiter to use")
+	trimSpacesOption = flag.Bool("t", false, "Set to true to trim whitespace")
 )
 
 func main() {
 	flag.Parse()
 
+	if *versionOption {
+		fmt.Printf("Transpose %s\nCommit: %s\nDate: %s", version, commit, date)
+		os.Exit(0)
+	}
+
 	options := transpose.Options{
-		Delimiter:  *delimiter,
-		TrimSpaces: *trimSpaces,
+		Delimiter:  *delimiterOption,
+		TrimSpaces: *trimSpacesOption,
 	}
 
 	if err := transpose.Transpose(os.Stdin, os.Stdout, options); err != nil {
